bc647edc0348ac4ac2dbad401b05c0b046998e56
Bipul Dubey <xs329-bipdub@xs-host603-027.xs.neuralcompany.team>
2023-07-14 14:13:31 +0530

    final mi_card

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -10,15 +10,75 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         body:SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 55.0,
                 backgroundImage: AssetImage('images/profile_image.png'),
+              ),
+              const Text(
+                "Bipul Dubey",
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: 'SourceSans',
+                  fontSize: 22,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5
+                ),
+              ),
+              Divider(
+                thickness: 1,
+                color: Colors.teal.shade50,
+                indent: 120,
+                endIndent: 120,
+              ),
+              const Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
+                child: ListTile(
+                    leading: Icon(
+                        Icons.phone,
+                    ),
+                    title: Text(
+                      "+91 123 1234 123",
+                      style: TextStyle(
+                        fontFamily: "SourceSans",
+                        fontSize: 20
+                      ),
+                    ),
+                  iconColor: Colors.teal,
+                  textColor: Colors.teal,
+                ),
+              ),
+              const Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                  ),
+                  title: Text(
+                    "bipul@gmail.com",
+                    style: TextStyle(
+                        fontFamily: "SourceSans",
+                        fontSize: 20
+                    ),
+                  ),
+                  iconColor: Colors.teal,
+                  textColor: Colors.teal,
+                )
               ),
             ],
           ),
